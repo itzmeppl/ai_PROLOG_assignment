018f8a8222a5c7cb491a677034d8f47754bf903f
c <96211609+burmys@users.noreply.github.com>
2023-09-19 22:55:06 -0400

    Update q3_collab.pl

--- q3_collab.pl
+++ q3_collab.pl
@@ -15,9 +15,38 @@
 %%%%% SECTION: articleAuthor
 % Put your atomic statements for the articleAuthor predicate in this section
 
+articleAuthor(communist_manifesto,karl_marx).
+articleAuthor(very_hungry_catipillar,eric_carle).
+articleAuthor(jane_eyre,charlotte_bronte).
+articleAuthor(villette,charlotte_bronte).
+articleAuthor(matilda,roald_dahl).
+articleAuthor(the_bfg,roald_dahl).
+articleAuthor(two_treatises_of_government,john_locke).
+articleAuthor(ninety_five_theses,martin_luther).
+articleAuthor(the_social_contract,jean_jacques_rousseau).
+articleAuthor(discourse_on_inequality,jean_jacques_rousseau).
+articleAuthor(holes,louis_sachar).
+articleAuthor(the_birchbark_house,louise_erdrich).
+articleAuthor(animal_farm,george_orwell).
 
 %%%%% SECTION: articleTopic
 % Put your atomic statements for the articleTopic predicate in this section
+
+articleTopic(communist_manifesto,philosophy).
+articleTopic(very_hungry_catipillar,fiction).
+articleTopic(jane_eyre,fiction).
+articleTopic(villette,fiction).
+articleAuthor(the_road,post_acopalyptic).
+articleAuthor(matilda,fiction).
+articleAuthor(the_bfg,fiction).
+articleAuthor(two_treatises_of_government,politics).
+articleAuthor(ninety_five_theses,theology).
+articleAuthor(the_social_contract,politics).
+articleAuthor(discourse_on_inequality,politics).
+articleAuthor(holes,fiction).
+articleAuthor(the_birchbark_house,fiction).
+articleAuthor(animal_farm,satire).
+
 
 
 %%%%% SECTION: collabDist
