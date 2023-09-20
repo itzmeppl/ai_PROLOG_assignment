2ce7aac57902fcfe98d2448b61c049fae4ab9f75
c <96211609+burmys@users.noreply.github.com>
2023-09-20 13:14:43 -0400

    Update q3_collab.pl

--- q3_collab.pl
+++ q3_collab.pl
@@ -2,9 +2,9 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
-%%%%% NAME:
-%%%%% NAME:
+%%%%% NAME: Riddhima Garg
+%%%%% NAME: Wendy Xie
+%%%%% NAME: Claudia Chan
 %
 % Add the required rules in the corresponding sections. 
 % If you put the rules in the wrong sections, you will lose marks.
