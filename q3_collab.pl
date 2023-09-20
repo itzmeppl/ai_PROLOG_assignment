31a2f5c71d7e1d2c2aca6b53889b6cc579c4b4d0
c <96211609+burmys@users.noreply.github.com>
2023-09-19 23:00:00 -0400

    Update q3_collab.pl

--- q3_collab.pl
+++ q3_collab.pl
@@ -15,18 +15,18 @@
 %%%%% SECTION: articleAuthor
 % Put your atomic statements for the articleAuthor predicate in this section
 
-articleAuthor(communist_manifesto,karl_marx).
+articleAuthor(communist_manifesto,karl_marx,john_locke).
 articleAuthor(very_hungry_catipillar,eric_carle).
 articleAuthor(jane_eyre,charlotte_bronte).
 articleAuthor(villette,charlotte_bronte).
 articleAuthor(the_road,cormac_mccarthy).
-articleAuthor(matilda,roald_dahl).
+articleAuthor(matilda,roald_dahl,george_orwell).
 articleAuthor(the_bfg,roald_dahl).
 articleAuthor(two_treatises_of_government,john_locke).
-articleAuthor(ninety_five_theses,martin_luther).
+articleAuthor(ninety_five_theses,martin_luther,karl_marx).
 articleAuthor(the_social_contract,jean_jacques_rousseau).
 articleAuthor(discourse_on_inequality,jean_jacques_rousseau).
-articleAuthor(holes,louis_sachar).
+articleAuthor(holes,louis_sachar,cormac_mccarthy).
 articleAuthor(the_birchbark_house,louise_erdrich).
 articleAuthor(animal_farm,george_orwell).
 
