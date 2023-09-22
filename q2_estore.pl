b05bf257fc662795e1bc0d0aa07c8283c280b2e5
wendyxie19 <128992104+wendyxie19@users.noreply.github.com>
2023-09-22 16:08:04 -0400

    Update q2_estore.pl

--- q2_estore.pl
+++ q2_estore.pl
@@ -61,6 +61,10 @@ Sub is Laptotal + Montotal +  Keybrtotal.
 %%%%% SECTION: calculateBaseShipping
 % Put your rules for the calculateBaseShipping predicate in this section
 
+calculateBaseShipping(ShippingCost) :- shippingCost(laptop,Lapship),numPurchased(laptop,Lapcount),Lapshiptotal is Lapship * Lapcount,
+shippingCost(monitor,Monship), numPurchased(monitor,Moncount), Monshiptotal is Moncost * Monship,
+shippingCost(keyboard,Keybrship), numPurchased(keyboard,Keybrcount), Keybrshiptotal is Keybrship * Keybrcount,
+ShippingCost is Lapshiptotal + Monshiptotal +  Keybrhshiptotal.  
 
 %%%%% SECTION: calculateShipping
 % Put your rules for the calculateShipping predicate in this section
