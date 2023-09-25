a9d112b093ea981f38e986452bf51d92da2bb789
Riddhima <60081089+itzmeppl@users.noreply.github.com>
2023-09-24 23:16:10 -0400

    base cases

--- q3_collab.pl
+++ q3_collab.pl
@@ -53,6 +53,12 @@ articleTopic(animal_farm,satire).
 %%%%% SECTION: collabDist
 % Put your rules for collabDist in this section
 
+%these account for the base cases where A1 and A2 is itself or has a direct connection
+collabDist(A1, A2, MaxDist) :- articleAuthor(X, A1), articleAuthor(X, A2), MaxDist == 0.
+collabDist(A1, A2, MaxDist) :- articleAuthor(X, A1), articleAuthor(X, A2), A1 \= A2, MaxDist == 1.
+
+
+
 
 %%%% SECTION: collabDistWithAI
 % Put your rules for collabDistWithAI in this section
